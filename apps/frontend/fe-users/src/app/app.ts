28cd6f0b54d846eb56d312e9119d487b01d9a18a
SergiiMost <s.v.mostovoyi@gmail.com>
2026-08-18 20:06:53 -0400

    apps

--- apps/frontend/fe-users/src/app/app.ts
+++ apps/frontend/fe-users/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { Header } from '@nx-multi-setup/header';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, Header],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
